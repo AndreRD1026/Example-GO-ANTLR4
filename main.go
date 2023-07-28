4445a96aa510bb4ee0c4965e0b8e69aa6cf23d5b
AndreRD1026 <davilacesar125@gmail.com>
2023-07-27 18:13:01 -0600

    Calc Example

---
+++ main.go
@@ -0,0 +1,97 @@
+// example.go
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"main/parser"
+
+	"github.com/antlr4-go/antlr/v4"
+)
+
+type calcListener struct {
+	*parser.BaseCalcListener
+
+	stack []int
+}
+
+func (l *calcListener) push(i int) {
+	l.stack = append(l.stack, i)
+}
+
+func (l *calcListener) pop() int {
+	if len(l.stack) < 1 {
+		panic("stack is empty unable to pop")
+	}
+
+	// Get the last value from the stack.
+	result := l.stack[len(l.stack)-1]
+
+	// Pop the last element from the stack.
+	l.stack = l.stack[:len(l.stack)-1]
+
+	return result
+}
+
+// ExitMulDiv is called when exiting the MulDiv production.
+func (l *calcListener) ExitMulDiv(c *parser.MulDivContext) {
+	right, left := l.pop(), l.pop()
+
+	switch c.GetOp().GetTokenType() {
+	case parser.CalcParserMUL:
+		l.push(left * right)
+	case parser.CalcParserDIV:
+		l.push(left / right)
+	default:
+		panic(fmt.Sprintf("unexpected operation: %s", c.GetOp().GetText()))
+	}
+}
+
+// ExitAddSub is called when exiting the AddSub production.
+func (l *calcListener) ExitAddSub(c *parser.AddSubContext) {
+	right, left := l.pop(), l.pop()
+
+	switch c.GetOp().GetTokenType() {
+	case parser.CalcParserADD:
+		l.push(left + right)
+	case parser.CalcParserSUB:
+		l.push(left - right)
+	default:
+		panic(fmt.Sprintf("unexpected operation: %s", c.GetOp().GetText()))
+	}
+}
+
+// ExitNumber is called when exiting the Number production.
+func (l *calcListener) ExitNumber(c *parser.NumberContext) {
+	i, err := strconv.Atoi(c.GetText())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	l.push(i)
+}
+
+// calc takes a string expression and returns the evaluated result.
+func calc(input string) int {
+	// Setup the input
+	//var p *parser.MySqlParser
+	is := antlr.NewInputStream(input)
+
+	// Create the Lexer
+	lexer := parser.NewCalcLexer(is)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Create the Parser
+	p := parser.NewCalcParser(stream)
+
+	// Finally parse the expression (by walking the tree)
+	var listener calcListener
+	antlr.ParseTreeWalkerDefault.Walk(&listener, p.Start_())
+
+	return listener.pop()
+}
+
+func main() {
+	fmt.Printf("The answer is: %d\n", calc("1 + 3 * 3"))
+}
